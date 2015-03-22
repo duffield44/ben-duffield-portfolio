fc678f0a5e8f812ce2c257ffd3097e87e5eb176b
duffield44 <duffieldbenjamin44@gmail.com>
2015-03-22 11:50:42 +0800

    Change placeholder images and their width. Initiate ajax call for items.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(function(){
 
+	// Call Isotope plugin with imagesloaded
 	var $container = $('#container');
 
 	$container.imagesLoaded( function() {
@@ -7,6 +8,17 @@ $(function(){
     		itemSelector: '.item',
 	  		layoutMode: 'masonry'
   		});
+	});
+
+	// Call items stored in items.js
+	$.ajax({
+		url: "js/items.js",
+		contentType: "application/json",
+		dataType: "json",
+		type: "GET"
+	})
+	.success(function(result){
+		console.log(result);
 	});
 
 });
