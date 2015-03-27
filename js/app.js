2d10d6098679b245afb6b084011d6dda0ad97811
duffield44 <duffieldbenjamin44@gmail.com>
2015-03-27 18:57:01 +0800

    Add urls to items. Add functionality when user clicks on an item.

--- js/app.js
+++ js/app.js
@@ -6,67 +6,89 @@ $(function(){
 			id: "1",
 			itemName: "Shopping List",
 			itemImage: "images/shopping.jpg",
-			category: "App"
+			category: "App",
+			button: "Make Your List",
+			url: "http://duffield44.github.io/shopping-list/"
 		},
 		{
 			id: "2",
 			itemName: "jQuery Street Fighter",
 			itemImage: "images/ryu.jpg",
-			category: "App"
+			category: "App",
+			button: "Fight!",
+			url: "http://duffield44.github.io/jquery-streetfighter/"
 		},
 		{
 			id: "3",
 			itemName: "Price My Ride",
 			itemImage: "images/price-my-ride.png",
-			category: "App"
+			category: "App",
+			button: "Price Your Ride",
+			url: "http://duffield44.github.io/premierleague-quiz"
 		},
 		{
 			id: "4",
 			itemName: "Hot or Cold",
 			itemImage: "images/hot-or-cold.jpg",
-			category: "App"
+			category: "App",
+			button: "Play Hot or Cold",
+			url: "http://duffield44.github.io/hot-or-cold-app/"
 		},
 		{
 			id: "5",
 			itemName: "Premier League Quiz",
 			itemImage: "images/premierleague-quiz.png",
-			category: "App"
+			category: "App",
+			button: "Test Your Knowledge",
+			url: "http://duffield44.github.io/premierleague-quiz"
 		},
 		{
 			id: "6",
 			itemName: "My Special Dress",
 			itemImage: "images/my-special-dress.jpg",
-			category: "Company"
+			category: "Company",
+			button: "Find Your Dress",
+			url: "http://myspecialdress.com/"
 		},
 		{
 			id: "7",
 			itemName: "Ubud Pilates Club",
 			itemImage: "images/ubud-pilates-club.jpg",
-			category: "Company"
+			category: "Company",
+			button: "Go To Website",
+			url: "http://ubudpilatesclub.com/"
 		},
 		{
 			id: "8",
 			itemName: "Hypnotism Pro",
 			itemImage: "images/hypnotismpro.jpg",
-			category: "Company"
+			category: "Company",
+			button: "Go To Website",
+			url: "http://hypnotismpro.com/"
 		},
 		{
 			id: "9",
 			itemName: "Karma Clone",
 			itemImage: "images/karma.jpg",
-			category: "Website"
+			category: "Website",
+			button: "Go To Website",
+			url: "http://duffield44.github.io/startup-landing-page-clone/"
 		},
 		{
 			id: "10",
 			itemName: "Blasting Off with Bootstrap",
 			itemImage: "images/bootstrap.jpg",
-			category: "Website"
+			category: "Website",
+			button: "Blast Off!",
+			url: "http://duffield44.github.io/bootstrap/"
 		},
 		{
 			id: "11",
 			itemName: "Fact vs Fitness",
 			itemImage: "images/fact-vs-fitness.jpg",
-			category: "Website"
+			category: "Website",
+			button: "Go To Website",
+			url: "http://www.factvsfitness.com/"
 		}
 	]	
 
@@ -88,6 +110,9 @@ $(function(){
 			// Add Category;
 			result.addClass(items[i].category);
 			result.find('.category').text(items[i].category);
+
+			// Add ID
+			result.attr('id', items[i].id);
 
 			// Append to container
 			$('#container').append(result);
@@ -155,6 +180,29 @@ $(function(){
 	})
 	.mouseleave(function(){
 		$(this).find('.hover').hide();
+	});
+
+	// CLICK ON ITEM
+	$('.item').on('click', function(){
+
+		// Clone .description div
+		var desc = $('.templates .description').clone();
+		var itemID = $(this).attr('id');
+
+		// Add Image
+		desc.find('img').attr('src', items[itemID - 1].itemImage);	
+
+		// Add Header, Category & Description
+		desc.find('h1').text(items[itemID - 1].itemName);
+		desc.find('h3').text(items[itemID - 1].category);
+		desc.find('p').text(items[itemID - 1].itemDesc);
+
+		// Add Button
+		desc.find('.btn-go').text(items[itemID - 1].button).attr('href', items[itemID - 1].url);
+
+		// Append to #descContainer section
+		$('#descContainer').empty().append(desc);
+		$(desc).slideDown(400);
 	});
 
 	// INTRO CLOSE BUTTON
